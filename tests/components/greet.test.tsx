96db233b9a4783896fa0967743e6f938f5e59269
Ranjith.Kumar <theranjith@gmail.com>
2024-07-06 22:34:37 +0530

    test rendering components

---
+++ tests/components/greet.test.tsx
@@ -0,0 +1,25 @@
+import { it, expect, describe } from 'vitest'
+import { render,screen } from '@testing-library/react'
+import Greet from '../../src/components/Greet'
+import '@testing-library/jest-dom/vitest'
+
+describe('Greet', () => {
+    it('should render Hello with name when name is provided', () => {
+        render(<Greet name="Ranjith" />);
+        
+        //screen.debug();//it logs the component in console log
+        const heading= screen.getByRole("heading");
+        expect(heading).toBeInTheDocument(); 
+        expect(heading).toHaveTextContent(/hello ranjith/i);
+
+    })
+
+    it('should render login button when name is not provided', () => {
+        render(<Greet/>);
+        
+        const button= screen.getByRole("button");
+        expect(button).toBeInTheDocument(); 
+        expect(button).toHaveTextContent(/login/i);
+
+    })
+})
