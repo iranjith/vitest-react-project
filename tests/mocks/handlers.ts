aa78eaa38f8db3cef3e6f6020f396d9838e92c06
Ranjith.Kumar <theranjith@gmail.com>
2024-11-11 07:04:41 +0530

    tested product list data fetching

--- tests/mocks/handlers.ts
+++ tests/mocks/handlers.ts
@@ -17,4 +17,20 @@ export const handlers = [
       },
     ]);
   }),
+  http.get("/products", () => {
+    return HttpResponse.json([
+      {
+        id: 1,
+        name: "Product 1",
+      },
+      {
+        id: 2,
+        name: "Product 2",
+      },
+      {
+        id: 3,
+        name: "Product 3",
+      },
+    ]);
+  }),
 ];
