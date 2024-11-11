59b9cc81e29368b71a3785c77eaa51c118798f72
Ranjith.Kumar <theranjith@gmail.com>
2024-11-11 06:51:05 +0530

    Setup mock service worker

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,7 +1,11 @@
-import { it, expect, describe } from 'vitest'
+import { it, expect, describe } from "vitest";
 
-describe('group', () => {
-    it('should', () => {
-        expect(1).toBeTruthy();
-    })
-})
+describe("group", () => {
+  it("should", async () => {
+    const response = await fetch("/categories");
+    const data = await response.json();
+    console.log(data);
+    expect(data).toHaveLength(3);
+    expect(1).toBeTruthy();
+  });
+});
