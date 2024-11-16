bbb7eb6a11349f6bf4c0bfa2770f4cee77be6104
Ranjith.Kumar <theranjith@gmail.com>
2024-11-16 22:17:45 +0530

    Added mock data and handlers

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,11 +1,9 @@
-import { it, expect, describe } from "vitest";
+import { describe, it } from "vitest";
+import { db } from "./mocks/db";
 
 describe("group", () => {
   it("should", async () => {
-    const response = await fetch("/categories");
-    const data = await response.json();
-    console.log(data);
-    expect(data).toHaveLength(3);
-    expect(1).toBeTruthy();
+    const product = db.product.create({ name: "Apple" });
+    console.log(db.product.delete({ where: { id: { equals: product.id } } }));
   });
 });
